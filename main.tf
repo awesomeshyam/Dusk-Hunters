089b305aed220387292b4bed6160dd459601aff3
NISHU <awesomeshyam91@gmail.com>
2024-12-27 14:56:29 +0530

    updated-rg

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "zelectric-rgs"
+  location = "westus"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "cat-branch"
+  location = "westus"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "hunter-branch"
+  location = "westus"
+}
